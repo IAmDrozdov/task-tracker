c04212924c7692a3b2b9874c107d1e02555e7ccd
Sasha Sasha <aleksandr.drozdov.99@gmail.com>
2018-04-06 20:04:33 +0300

    updated arguments parser

--- main.py
+++ main.py
@@ -15,28 +15,26 @@ def main():
     parser = create_parser()
     namespace = parser.parse_args()
     ######################################
-    if namespace.command == 'add':
-        info_new = namespace.description
-        id_new = randomizer.get_actual_index(container)
-        if namespace.deadline:
-            deadline_new = datetime_parser.get_deadline(namespace.deadline)
-        else:
-            deadline_new = None
-        new_task = Task(info=info_new, id=id_new, deadline=deadline_new, tags=namespace.tags.split())
-        container.append(new_task)
-        database.serialize(container, 'database_tasks.json')
+    if namespace.target == 'task':
+        if namespace.command == 'add':
+            info_new = namespace.description
+            id_new = randomizer.get_actual_index(container)
+            deadline_new = datetime_parser.get_deadline(namespace.deadline) if namespace.deadline else None
+            tags_new = namespace.tags.split() if namespace.tags else []
+            new_task = Task(info=info_new, id=id_new, deadline=deadline_new, tags=tags_new)
+            container.append(new_task)
+            database.serialize(container, 'database_tasks.json')
+        #######################################
+        elif namespace.command == 'remove':
+            Task.delete(container, namespace.id)
+        #######################################
+        elif namespace.command == 'show':
+            if namespace.id is not None:
+                Task.print(container, namespace.id)
+            else:
+                Task.print(container)
     #######################################
-    elif namespace.command == 'remove':
-        Task.delete(container, namespace.id)
-
-    #######################################
-    elif namespace.command == 'show':
-        if namespace.id is not None:
-            Task.print(container, namespace.id)
-        else:
-            Task.print(container)
-    #######################################
-    elif namespace.command == 'calendar':
+    elif namespace.target == 'calendar':
         calendar_custom.print_month_calendar(container, namespace.date[0], namespace.date[1])
 
 
--- parser.py
+++ parser.py
@@ -3,19 +3,23 @@ import argparse
 
 def create_parser():
     parser = argparse.ArgumentParser()
-    subparsers = parser.add_subparsers(dest='command')
+    subparser_targets = parser.add_subparsers(dest='target')
 
-    add_parser = subparsers.add_parser('add')
-    add_parser.add_argument('-d', '--description', required=True)
-    add_parser.add_argument('-dl', '--deadline')
-    add_parser.add_argument('-t', '--tags')
+    targets_task = subparser_targets.add_parser('task')
+    task_parser = targets_task.add_subparsers(dest='command')
+    add_task = task_parser.add_parser('add')
+    add_task.add_argument('-d', '--description', required=True)
+    add_task.add_argument('-dl', '--deadline')
+    add_task.add_argument('-t', '--tags')
 
-    show_parser = subparsers.add_parser('show')
-    show_parser.add_argument('id', nargs='?', action='store', type=int)
+    show_task = task_parser.add_parser('show')
+    show_task.add_argument('id', nargs='?', action='store', type=int)
 
-    remove_parser = subparsers.add_parser('remove')
-    remove_parser.add_argument('id', type=int)
+    remove_task = task_parser.add_parser('remove')
+    remove_task.add_argument('id', type=int)
 
-    calendar_parser = subparsers.add_parser('calendar')
-    calendar_parser.add_argument('date', type=int, nargs=2)
+    targets_calendar = subparser_targets.add_parser('calendar')
+    calendar_parser = targets_calendar.add_subparsers(dest='command')
+    show_calendar = calendar_parser.add_parser('show')
+    show_calendar.add_argument('date', type=int, nargs=2)
     return parser
