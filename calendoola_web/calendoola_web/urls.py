a0fb8c5a81dd200f70d52ee49bf5f252f9268f10
iamdrozdov <aleksandr.drozdov.99@gmail.com>
2018-06-13 22:55:26 +0300

    Began making authentification. Created login view

--- calendoola_web/calendoola_web/urls.py
+++ calendoola_web/calendoola_web/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url, include
 
 urlpatterns = [
-    url(r'^', include('caleweb.urls'))
+    url(r'^', include('caleweb.urls')),
+    url('^', include('django.contrib.auth.urls')),
 ]
